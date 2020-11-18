e1120b9d2e8f269a05414f8b623974c8dbfb7853
ARTAUD Grégory <gregory.rtd@gmail.com>
2020-11-18 17:01:01 +0100

    rm ft_fill from ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,39 +6,11 @@
 /*   By: gartaud <gartaud@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 15:38:44 by gartaud           #+#    #+#             */
-/*   Updated: 2020/11/18 16:43:53 by gartaud          ###   ########lyon.fr   */
+/*   Updated: 2020/11/18 17:00:21 by gartaud          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-void	ft_fill(char **s1, char *s2, int *i)
-{
-	char	*sub;
-	char	*tmp;
-	int		sub_ln;
-	int		tmp_ln;
-
-	if ((tmp = ft_strchr(s2, '%')))
-		sub_ln = tmp - s2;
-	else
-		sub_ln = ft_strlen(s2);
-	if (!sub_ln)
-		return ;
-	sub = ft_substr(s2, 0, sub_ln);
-	tmp_ln = ft_strlen(*s1) + sub_ln;
-	tmp = malloc(sizeof(char) * (tmp_ln + 1));
-	if (!tmp)
-		return ;
-	ft_bzero(tmp, tmp_ln);
-	ft_strlcpy(tmp, *s1, ft_strlen(*s1) + 1);
-	ft_strlcat(tmp, sub, tmp_ln + 1);
-	free(*s1);
-	free(sub);
-	*s1 = tmp;
-	*i += sub_ln - 1;
-	return ;
-}
 
 int		ft_printf(const char *s, ...)
 {
